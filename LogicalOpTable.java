a2a36e9f8430b1b9d34463176fb79cad6a4903c3
EszterSzollosiMaruzs <maruzseszti@freemail.hu>
2023-03-06 00:17:17 +0100

    Kezdés

---
+++ LogicalOpTable.java
@@ -0,0 +1,16 @@
+
+package logicaloptable;
+
+public class LogicalOpTable {
+   
+    public static void main(String[] args)   {
+       
+       System.out.println("A program " + args.length + " db parancssori argumentumot kapott:");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println((i+1) + ". argumentum: " + args[i]);
+        }
+    }
+                     
+}
+    
+
